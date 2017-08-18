8dfb0053f0a23b4a3ac96948433633871cfca52f
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-08-18 13:56:36 -0700

    Change setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/csurfer/heat',
+    url='https://github.com/csurfer/pyheatmagic',
 
     # Author details
     author='Vishwas B Sharma',
