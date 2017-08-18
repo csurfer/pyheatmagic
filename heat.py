5bd96563e2d3a659a9a86d3fed4c9334610d7204
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-08-18 16:08:38 -0700

    Add missing comments in IPython magic

--- heat.py
+++ heat.py
@@ -32,6 +32,9 @@ from pyheat import PyHeat
 
 @magics_class
 class PyHeatMagic(Magics):
+    """Class with IPython magic commands to effectively use py-heat profiling
+    within IPython."""
+
     @magic_arguments.magic_arguments()
     @magic_arguments.argument(
         '-o',
@@ -41,6 +44,12 @@ class PyHeatMagic(Magics):
     )
     @cell_magic
     def heat(self, line, cell):
+        """Method to profile the python code in the ipython cell and display it
+        as a heatmap using py-heat package.
+
+        :param line: Line value for the ipython line this magic is called from.
+        :param cell: Cell value for the ipython cell this magic is called from.
+        """
         args = magic_arguments.parse_argstring(self.heat, line)
         filename = args.out
         if filename is not None:
